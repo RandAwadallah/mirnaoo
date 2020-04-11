bfacceb52cf945828e4c9a4dd84a71ffb3e5f259
ritaabuslemeh <46849451+ritaabuslemeh@users.noreply.github.com>
2020-04-11 12:02:08 +0300

    Initial commit

---
+++ PalPharmacyApp/src/main/java/com/app/palpharmacy/activities/MainActivity.java
@@ -0,0 +1,101 @@
+package com.app.palpharmacy.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.app.palpharmacy.R;
+import com.app.palpharmacy.adapters.RecyclerViewAdapter;
+import com.app.palpharmacy.model.Pharmacy;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String JSON_URL = "http://www.palpharmacy.com/index.php/getPharmacies" ;
+    private JsonArrayRequest request ;
+    private RequestQueue requestQueue ;
+    private List<Pharmacy> lstPharmacy;
+    private RecyclerView recyclerView ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lstPharmacy = new ArrayList<>() ;
+        recyclerView = findViewById(R.id.recyclerviewid);
+        jsonrequest();
+
+
+
+    }
+
+    private void jsonrequest() {
+
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                JSONObject jsonObject  = null ;
+
+                for (int i = 0 ; i < response.length(); i++ ) {
+
+
+                    try {
+                        jsonObject = response.getJSONObject(i) ;
+                        Pharmacy pharmacy = new Pharmacy() ;
+                        pharmacy.setName(jsonObject.getString("name_en"));
+                        pharmacy.setDescription(jsonObject.getString("address"));
+//                        pharmacy.setRating(jsonObject.getString("Rating"));
+//                        pharmacy.setCategorie(jsonObject.getString("categorie"));
+//                        pharmacy.setNb_episode(jsonObject.getInt("episode"));
+//                        pharmacy.setStudio(jsonObject.getString("studio"));
+                        pharmacy.setImage_url(jsonObject.getString("image"));
+                        lstPharmacy.add(pharmacy);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+
+                setuprecyclerview(lstPharmacy);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        requestQueue.add(request) ;
+
+
+    }
+
+    private void setuprecyclerview(List<Pharmacy> lstPharmacy) {
+
+
+        RecyclerViewAdapter myadapter = new RecyclerViewAdapter(this, lstPharmacy) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myadapter);
+
+    }
+}
