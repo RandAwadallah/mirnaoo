33ecf0bdd7ebfa8a5bd0d19f248f3c22f9c79d42
Mirna Abu Qube <mirna.qube@gmail.com>
2020-04-15 15:46:29 +0300

    drawer

--- PalPharmacyApp/src/main/java/com/app/palpharmacy/activities/MainActivity.java
+++ PalPharmacyApp/src/main/java/com/app/palpharmacy/activities/MainActivity.java
@@ -1,12 +1,23 @@
 package com.app.palpharmacy.activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -24,7 +35,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private final String JSON_URL = "http://www.palpharmacy.com/index.php/getPharmacies" ;
     private JsonArrayRequest request ;
@@ -32,14 +43,22 @@ public class MainActivity extends AppCompatActivity {
     private List<Pharmacy> lstPharmacy;
     private RecyclerView recyclerView ;
 
-
+private DrawerLayout drawer;
     RecyclerViewAdapter myadapter;
 EditText searchinput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        searchinput= findViewById(R.id.edittext);
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer= findViewById(R.id.draw_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+drawer.addDrawerListener(toggle);
+toggle.syncState();
+searchinput= findViewById(R.id.edittext);
 
         lstPharmacy = new ArrayList<>() ;
         recyclerView = findViewById(R.id.recyclerviewid);
@@ -47,6 +66,16 @@ EditText searchinput;
 
 
 
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     private void jsonrequest() {
@@ -118,5 +147,28 @@ searchinput.addTextChangedListener(new TextWatcher() {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(myadapter);
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_aboutus:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new aboutusfragment()).commit();
+                break;
+            case R.id.nav_language:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Languagefragment()).commit();
+                break;
+            case R.id.nav_notification:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new notificationfragment()).commit();
+                break;
+            case R.id.nav_location:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new locationfragment()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this,"Share",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
